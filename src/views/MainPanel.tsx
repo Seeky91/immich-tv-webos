12a76522d2dd9107f2c2242f74bf345e3b4af2ac
Quentin Jean-Amans <quentin.sqja@gmail.com>
2026-05-15 18:32:15 +0200

    [Refactor] - stabilize pagination object in MainPanel to prevent spurious useEffect fires (IDM-007)

--- src/views/MainPanel.tsx
+++ src/views/MainPanel.tsx
@@ -13,6 +13,10 @@ const MainPanel: React.FC<MainPanelProps> = ({contentWidth}) => {
 	const {data, isLoading, isError, error, fetchNextPage, hasNextPage, isFetchingNextPage, allBuckets} = useInfiniteTimeline();
 
 	const loadedGroups = useMemo(() => data?.pages.flatMap((p) => p.groups) ?? [], [data]);
+	const pagination = useMemo(
+		() => ({allBuckets, hasNextPage: !!hasNextPage, isFetchingNextPage, fetchNextPage}),
+		[allBuckets, hasNextPage, isFetchingNextPage, fetchNextPage]
+	);
 
 	return (
 		<QueryStateView isLoading={isLoading} error={isError ? error : null}>
@@ -20,7 +24,7 @@ const MainPanel: React.FC<MainPanelProps> = ({contentWidth}) => {
 				groups={loadedGroups}
 				contentWidth={contentWidth}
 				style={{paddingRight: ri.scale(GRID_INSET_RIGHT_PX)}}
-				pagination={{allBuckets, hasNextPage: !!hasNextPage, isFetchingNextPage, fetchNextPage}}
+				pagination={pagination}
 			/>
 		</QueryStateView>
 	);
